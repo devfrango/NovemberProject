be19a1531ee7c3067ab0c21aeed2aa5eb7cb2e02
Frankie(devfrango) <devfrango@gmail.com>
2020-11-01 23:23:33 +0700

    update batmobile

--- src/main/java/BatMan.java
+++ src/main/java/BatMan.java
@@ -3,5 +3,7 @@ public class BatMan {
     public static void main(String[] args) {
         System.out.println("This is Batman!");
         System.out.print("hellO!");
+
+        //batman has a coold batmobile
     }
 }
