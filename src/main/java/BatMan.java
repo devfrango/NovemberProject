0233a13e702c8bdaf2602a4ec6722af9ce811c20
Frankie(devfrango) <devfrango@gmail.com>
2020-11-01 22:52:12 +0700

    update

--- src/main/java/BatMan.java
+++ src/main/java/BatMan.java
@@ -2,6 +2,6 @@ public class BatMan {
 
     public static void main(String[] args) {
         System.out.println("This is Batman!");
-        System.out.print("hellO!"))
+        System.out.print("hellO!");
     }
 }
